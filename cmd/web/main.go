f3721a464b24372441acad7caf84b3ef2a6b0502
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-09-02 17:57:51 -0500

    initialize fields for application, get flags and envs, set up info and error loggers, and create map for template cache

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"html/template"
 	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 const version = "1.0.0"
 const cssVersion = "1"
 
 type config struct {
-	port uint
+	port int
 	env  string
 	api  string
 	db   struct {
@@ -29,6 +34,48 @@ type application struct {
 	version       string
 }
 
-func main() {
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:              fmt.Sprintf("%:d", app.config.port),
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+	}
 
+	app.infoLog.Println("Starting HTTP server in %s mode on port %d", app.config.env, app.config.port)
+
+	return srv.ListenAndServe()
+}
+
+func main() {
+	var cfg config
+
+	// read flags into config variable
+	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Application environment {development|production}")
+	flag.StringVar(&cfg.api, "api", "http://localhost:4001", "URL to api")
+	flag.Parse()
+
+	cfg.stripe.key = os.Getenv("STRIPE_KEY")
+	cfg.stripe.secret = os.Getenv("STRIPE_SECRET")
+
+	// set up loggers
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// create map for template cache
+	tc := make(map[string]*template.Template)
+
+	// initialize application fields
+	app := &application{
+		config:        cfg,
+		infoLog:       infoLog,
+		errorLog:      errorLog,
+		templateCache: tc,
+		version:       version,
+	}
+
+	// initialize web server
 }
