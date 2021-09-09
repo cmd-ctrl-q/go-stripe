1e16a4055f7412c30e2ea59da7dc6b9ea1d96887
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-09-09 15:29:34 -0500

    create db connection pool and dsn flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/cmd-ctrl-q/go-stripe/internal/driver"
 )
 
 const version = "1.0.0"
@@ -55,13 +57,12 @@ func main() {
 	// read flags into config variable
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment {development|production}")
+	flag.StringVar(&cfg.db.dsn, "dsn", "root:password@tcp(localhost:3306)/widgets?parseTime=true&tls=false", "DSN")
 	flag.StringVar(&cfg.api, "api", "http://localhost:4001", "URL to api")
 	flag.Parse()
 
 	key := os.Getenv("STRIPE_KEY")
 	secret := os.Getenv("STRIPE_SECRET")
-	log.Println(key)
-	log.Println(secret)
 	if key == "" || secret == "" {
 		log.Panic("Stripe key or secret is empty. (Possible fixes: .air.toml, Makefile, local env variables)")
 	}
@@ -71,7 +72,14 @@ func main() {
 
 	// set up loggers
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile|log.Lmsgprefix)
+
+	// create db connection pool
+	conn, err := driver.OpenDB(cfg.db.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer conn.Close()
 
 	// create map for template cache
 	tc := make(map[string]*template.Template)
@@ -86,7 +94,7 @@ func main() {
 	}
 
 	// initialize web server
-	err := app.serve()
+	err = app.serve()
 	if err != nil {
 		app.errorLog.Println(err)
 		log.Fatal(err)
