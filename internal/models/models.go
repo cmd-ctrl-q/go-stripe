802668f282b1829a169294ea3167487726933977
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-09-11 22:53:31 -0500

    create query function InsertTransaction into db

--- internal/models/models.go
+++ internal/models/models.go
@@ -115,3 +115,37 @@ func (m *DBModel) GetWidget(id int) (Widget, error) {
 
 	return widget, nil
 }
+
+// InsertTransaction inserts a new txn and returns its id
+func (m *DBModel) InsertTransaction(tnx Transaction) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `
+		insert into transactions 
+			(amount, currency, last_four, bank_return_code, 
+			transaction_status_id, created_at, updated_at)
+		values (?, ?, ?, ?, ?, ?, ?)
+	`
+
+	result, err := m.DB.ExecContext(ctx, stmt,
+		tnx.Amount,
+		tnx.Currency,
+		tnx.LastFour,
+		tnx.BankReturnCode,
+		tnx.TransactionStatusID,
+		time.Now(),
+		time.Now(),
+	)
+	if err != nil {
+		return 0, err
+	}
+
+	// get last insert id
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+}
