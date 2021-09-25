7cd3df62fe23ab9cdccb70c59ce22aa248692fc0
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-09-25 13:46:32 -0500

    save sessions in database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/alexedwards/scs/mysqlstore"
 	"github.com/alexedwards/scs/v2"
 	"github.com/cmd-ctrl-q/go-stripe/internal/driver"
 	"github.com/cmd-ctrl-q/go-stripe/internal/models"
@@ -94,6 +95,7 @@ func main() {
 	// set up session
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
+	session.Store = mysqlstore.New(conn)
 
 	// create map for template cache
 	tc := make(map[string]*template.Template)
