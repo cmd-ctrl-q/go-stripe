5f944d569a0586ee6b401f777624bc27c5d36bdc
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-10-10 21:54:33 -0500

    delete subscription.page.tmpl and use the sale.page with StringMap params for sending data based on route

--- cmd/api/routes-api.go
+++ cmd/api/routes-api.go
@@ -38,7 +38,6 @@ func (app *application) routes() http.Handler {
 		r.Post("/all-subscriptions", app.AllSubscriptions)
 
 		r.Post("/get-sale/{id}", app.GetSale)
-		r.Post("/get-subscription/{id}", app.GetSubscription)
 	})
 
 	return mux
